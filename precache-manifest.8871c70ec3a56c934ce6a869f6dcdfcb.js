cc6e37ac100c99a68565e3651c76ea61df0aedaa
Ly-e <1091471466@qq.com>
2023-03-16 21:55:14 +0800

    deploy

---
+++ precache-manifest.8871c70ec3a56c934ce6a869f6dcdfcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f796dbc65d6e76958b2d",
+    "url": "/PinkBook-website/css/app.3590dce7.css"
+  },
+  {
+    "revision": "78a4f40a3dc3c91c87a754064331c32e",
+    "url": "/PinkBook-website/index.html"
+  },
+  {
+    "revision": "f796dbc65d6e76958b2d",
+    "url": "/PinkBook-website/js/app.18f015e2.js"
+  },
+  {
+    "revision": "a30d23bf0304232eb935",
+    "url": "/PinkBook-website/js/chunk-vendors.b03928da.js"
+  },
+  {
+    "revision": "7a44f2b381ca6c9460c10809040e7ff8",
+    "url": "/PinkBook-website/manifest.json"
+  },
+  {
+    "revision": "ac7e0904d1c18cec6910cd1c29e6cd8e",
+    "url": "/PinkBook-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/PinkBook-website/robots.txt"
+  }
+]);
